06262432998712a3e090073122df3c4d979a40d5
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-22 02:44:09 +0600

    home page and purchase page created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Pages/Home/Home'
+import Purchase from './Pages/Purchase';
+import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Manufacturere Website</h2>
+    <div className='max-w-7xl mx-auto'>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/purchase' element={<Purchase></Purchase>}></Route>
+      </Routes>
     </div>
   );
 }
