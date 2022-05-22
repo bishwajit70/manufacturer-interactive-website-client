f96b7cd4b36dc28263bd8015f4ea8e4e2be7038c
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-22 22:42:12 +0600

    review page design completed

--- src/Pages/Dashboard/AddAReview.js
+++ src/Pages/Dashboard/AddAReview.js
@@ -1,9 +1,54 @@
 import React from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useForm } from 'react-hook-form';
+import auth from '../../firebase.init';
 
 const AddAReview = () => {
+    const [user, loading, error] = useAuthState(auth);
+    const { register, handleSubmit, getValues, watch, formState: { errors } } = useForm();
+
+    const onSubmit = data => {
+        console.log(data)
+    }
     return (
         <div>
             <h2>Add A Review</h2>
+            <div className='mx-auto'>
+                <div className='grid lg:grid-cols-2 px-5 py-5 justify-between items-center'>
+                    <h2 className='mb-3'>Name: {user.displayName}</h2>
+                    <h2>Email: {user.email}</h2>
+                </div>
+                <div className="grid px-5 grid-cols-1 mx-auto">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                        <div>
+                            <label className="label">
+                                <span className="label-text">Review</span>
+                            </label>
+                            <textarea
+                                {...register("review", {
+                                    required: {
+                                        value: true,
+                                        message: 'Review is Required'
+                                    },
+                                })}
+                                name='review'
+                                type="text"
+                                placeholder="Your Review"
+                                className="border-2 w-11/12 rounded-lg p-2" cols="30" rows="10"></textarea>
+                                {/* <textarea name="" id="" cols="30" rows="10"></textarea> */}
+                            <label className="label">
+                                {errors.review?.type === 'required' && <span className="label-text-alt text-red-500">{errors.review.message}</span>}
+                            </label>
+                        </div>
+
+                        
+                        <div className='grid grid-cols-2 xs:w-full lg:grid-cols-2 gap-5 lg:w-11/12 justify-between'>
+                            <input className="btn" type="submit" value='Submit' />
+                            <input className="btn" type="submit" value='Update' />
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/Pages/Dashboard/MyProfile.js
+++ src/Pages/Dashboard/MyProfile.js
@@ -11,100 +11,103 @@ const MyProfile = () => {
         console.log(data)
     }
 
-
     return (
         <div className='mx-auto'>
-            <div className='flex py-5 justify-between items-center'>
-                <h2>Name: {user.displayName}</h2>
+            <div className='grid lg:grid-cols-2 px-5 py-5 justify-between items-center'>
+                <h2 className='mb-3'>Name: {user.displayName}</h2>
                 <h2>Email: {user.email}</h2>
             </div>
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <div>
-                    <label className="label">
-                        <span className="label-text">Education</span>
-                    </label>
-                    <input
-                        {...register("education", {
-                            required: {
-                                value: true,
-                                message: 'Education is Required'
-                            },
-                        })}
-                        name='education'
-                        type="text"
-                        placeholder="Your Education"
-                        className="border-2 w-full rounded-lg p-2" />
-                    <label className="label">
-                        {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
-                    </label>
-                </div>
+            <div className="grid px-5 grid-cols-1 mx-auto">
+                <form onSubmit={handleSubmit(onSubmit)}>
+                    <div>
+                        <label className="label">
+                            <span className="label-text">Education</span>
+                        </label>
+                        <input
+                            {...register("education", {
+                                required: {
+                                    value: true,
+                                    message: 'Education is Required'
+                                },
+                            })}
+                            name='education'
+                            type="text"
+                            placeholder="Your Education"
+                            className="border-2 w-11/12 rounded-lg p-2" />
+                        <label className="label">
+                            {errors.name?.type === 'required' && <span className="label-text-alt text-red-500">{errors.name.message}</span>}
+                        </label>
+                    </div>
 
-                <div>
-                    <label className="label">
-                        <span className="label-text">Location</span>
-                    </label>
-                    <input
-                        {...register("location", {
-                            required: {
-                                value: true,
-                                message: 'Location is Required'
-                            },
+                    <div>
+                        <label className="label">
+                            <span className="label-text">Location</span>
+                        </label>
+                        <input
+                            {...register("location", {
+                                required: {
+                                    value: true,
+                                    message: 'Location is Required'
+                                },
 
-                        })}
-                        name='location'
-                        type="text"
-                        placeholder="Your Location"
-                        className="border-2 w-full rounded-lg p-2" />
-                    <label className="label">
-                        {errors.location?.type === 'required' && <span className="label-text-alt text-red-500">{errors.location.message}</span>}
-                    </label>
-                </div>
+                            })}
+                            name='location'
+                            type="text"
+                            placeholder="Your Location"
+                            className="border-2 w-11/12 rounded-lg p-2" />
+                        <label className="label">
+                            {errors.location?.type === 'required' && <span className="label-text-alt text-red-500">{errors.location.message}</span>}
+                        </label>
+                    </div>
 
 
 
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Phone</span>
-                    </label>
-                    <input
-                        {...register("phone", {
-                            required: {
-                                value: true,
-                                message: 'Phone is Required'
-                            },
+                    <div className="">
+                        <label className="label">
+                            <span className="label-text">Phone</span>
+                        </label>
+                        <input
+                            {...register("phone", {
+                                required: {
+                                    value: true,
+                                    message: 'Phone is Required'
+                                },
 
-                        })}
-                        name='phone'
-                        type="text"
-                        placeholder="Your Phone"
-                        className="input input-bordered w-full max-w-xs" />
-                    <label className="label">
-                        {errors.phone?.type === 'required' && <span className="label-text-alt text-red-500">{errors.phone.message}</span>}
-                    </label>
-                </div>
-                <div className="form-control w-full max-w-xs">
-                    <label className="label">
-                        <span className="label-text">Linked In Profile Link</span>
-                    </label>
-                    <input
-                        {...register("linkedin", {
-                            required: {
-                                value: true,
-                                message: 'Linked is Required'
-                            },
+                            })}
+                            name='phone'
+                            type="text"
+                            placeholder="Your Phone"
+                            className="border-2 w-11/12 rounded-lg p-2" />
+                        <label className="label">
+                            {errors.phone?.type === 'required' && <span className="label-text-alt text-red-500">{errors.phone.message}</span>}
+                        </label>
+                    </div>
+                    <div className="">
+                        <label className="label">
+                            <span className="label-text">Linked In Profile Link</span>
+                        </label>
+                        <input
+                            {...register("linkedin", {
+                                required: {
+                                    value: true,
+                                    message: 'Linked is Required'
+                                },
 
-                        })}
-                        name='linkedin'
-                        type="text"
-                        placeholder="Your Linkedin Profile Link"
-                        className="input input-bordered w-full max-w-xs" />
-                    <label className="label">
-                        {errors.linkedin?.type === 'required' && <span className="label-text-alt text-red-500">{errors.linkedin.message}</span>}
-                    </label>
-                </div>
-                <input className="btn w-full max-w-xs" type="submit" value='Submit' />
-                <input className="btn w-full max-w-xs" type="submit" value='Update' />
-            </form>
+                            })}
+                            name='linkedin'
+                            type="text"
+                            placeholder="Your Linkedin Profile Link"
+                            className="border-2 w-11/12 rounded-lg p-2" />
+                        <label className="label">
+                            {errors.linkedin?.type === 'required' && <span className="label-text-alt text-red-500">{errors.linkedin.message}</span>}
+                        </label>
+                    </div>
+                    <div className='grid grid-cols-2 xs:w-full lg:grid-cols-2 gap-5 lg:w-11/12 justify-between'>
+                        <input className="btn" type="submit" value='Submit' />
+                        <input className="btn" type="submit" value='Update' />
+                    </div>
+                </form>
+            </div>
         </div>
     );
 };
