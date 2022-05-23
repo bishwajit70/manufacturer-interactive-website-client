f4e2d359b5eab75fda9532bf705ed4aff8836de7
bishwajit70 <mailtobishwa15@gmail.com>
2022-05-23 23:44:13 +0600

    review in home page done

--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const Reviews = () => {
+    const [reviews, setReviews] = useState([])
+    useEffect(() => {
+        fetch('http://localhost:5000/review')
+            .then(res => res.json())
+            .then(data => setReviews(data))
+
+    }, [])
+
     return (
         <div>
-            <h2>Review Part</h2>
+            <h2>Total Review Count: {reviews.length}</h2>
         </div>
     );
 };
--- src/Pages/Purchase.js
+++ src/Pages/Purchase.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useParams } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../firebase.init';
 import Footer from './Home/Footer';
 
@@ -52,7 +53,7 @@ const Purchase = () => {
             .then(res => res.json())
             .then(data => {
                 console.log(data)
-                alert("Order Placed Successfully.");
+                toast.success("Order Placed Successfully.")
                 event.target.reset();
             })
     }
